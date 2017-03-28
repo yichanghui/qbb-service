10a2853621489dc67ac6a33827775f75059a3f71
huxunqiang <huxunqiang@99114.com>
2017-03-28 18:16:23 +0800

    service jar

---
+++ src/main/java/com/hiveview/service/impl/IAreaImpl.java
@@ -0,0 +1,35 @@
+package com.hiveview.service.impl;
+
+import com.hiveview.dao.IAreaDao;
+import com.hiveview.entity.Area;
+import com.hiveview.service.IAreaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import utils.LevelUtil;
+
+import java.util.List;
+
+/**
+ * Created by hxq on 2017/3/13.
+ */
+@Service
+public class IAreaImpl implements IAreaService {
+
+    @Autowired
+    private IAreaDao areaDao;
+
+    @Override
+    public List<Area> getSonAreas(long parentId) {
+        Area area = new Area();
+        area.setParentId(parentId);
+        return areaDao.getList(area);
+    }
+
+    @Override
+    public List<Area> getAllOneLevel() {
+        Area area = new Area();
+        area.setLevel(LevelUtil.ONE_LEVEL.getVal());
+        return areaDao.getList(area);
+    }
+
+}
---
+++ src/main/java/utils/A.java
@@ -0,0 +1,7 @@
+package utils;
+
+/**
+ * Created by hxq on 2017/3/28.
+ */
+public class A {
+}
